e235441c00076b911086166644df3d9ca5144ccf
Nathan Pedowitz <npedowitz@yahoo.com>
2022-04-11 18:51:41 -0400

    added neo4j insert

--- packages/server/index.js
+++ packages/server/index.js
@@ -5,7 +5,8 @@ import querystring from 'query-string';
 import base64url from 'base64url';
 import randomstring from 'randomstring';
 import axios from 'axios';
-import e from 'express';
+import * as neo4j from 'neo4j-driver'
+
 
 dotenv.config();
 const app = express();
@@ -17,8 +18,17 @@ const redirect_uri = 'http://localhost:8080/';
 
 const code_verifier = randomstring.generate(128);
 
-const client_id = process.env.SPOTIFY_CLIENT_ID;
+// move to ENV
+const neo4j_username = process.env.NEO4J_USERNAME;
+const neo4j_password = process.env.NEO4J_PASSWORD;
+const neo4j_uri = process.env.DB_URI;
+
+
+const driver = neo4j.driver(neo4j_uri, neo4j.auth.basic(neo4j_username, neo4j_password))
+const session = driver.session()
+
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const client_id = process.env.SPOTIFY_CLIENT_ID;
 
 const base64Digest = crypto
   .createHash("sha256")
@@ -41,20 +51,48 @@ app.get('/', async (req, res) => {
                 code,
                 redirect_uri,
                 grant_type: 'authorization_code',
-                client_id: process.env.SPOTIFY_CLIENT_ID,
+                client_id,
                 code_verifier
             },
             headers: {
-                'Authorization':  `Basic ${Buffer.from(process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')}`,
+                'Authorization':  `Basic ${Buffer.from(client_id + ":" + client_secret).toString('base64')}`,
                 'Content-Type': 'application/x-www-form-urlencoded' 
             },
         };
 
-        await axios.post(authOptions.url, querystring.stringify(authOptions.form), {"headers": authOptions.headers}).then(async r => {
-            console.log(r.data)
-            await getUserProfile(r.data)
-        }).catch(r => console.error(r));
-        res.send("POGGERS")
+        const sessionDetails  = await axios.post(authOptions.url, querystring.stringify(authOptions.form), {"headers": authOptions.headers}).catch(r => console.error(r));
+        const userData = await getUserProfile(sessionDetails.data) // .id
+        console.log(userData)
+        const topTracks = await getUserTopTracks(sessionDetails.data)
+        const trackIds = topTracks.map(b => b.id).join(',')
+        const audioFeatures = await getAudioFeatures(sessionDetails.data, trackIds)
+        let mergedData = []
+        // insert into neo4j in for loop?
+
+        for (let i = 0; i < trackIds.length; i++){
+            // if !
+            if (!topTracks[i].name){
+                continue
+            }
+            //console.log(audioFeatures[i].id,topTracks[i].id) 
+            const mergeTrack = {...audioFeatures[i], ...topTracks[i] }
+            mergedData.push(mergeTrack)
+            // Running into an async error Cannot read property 'name' of undefined... working on it.
+            if (!mergeTrack.name){
+                continue
+            }
+            const ranking = i + 1
+            
+            const query = `MERGE (u:user {name: "${userData.id}"})
+                            MERGE (s:song {name: "${mergeTrack.name.replace(/"/g, '\'')}", acousticness: ${mergeTrack.acousticness}, danceability: ${mergeTrack.danceability}, energy: ${mergeTrack.energy}, tempo: ${mergeTrack.tempo}, valence: ${mergeTrack.valence}})
+                            MERGE (u)-[:LISTENS_TO {ranking: ${ranking}}]-(s)
+                            RETURN u, s`
+            
+            const writeResult = await session.writeTransaction(tx => tx.run (query, { userData, mergeTrack, ranking }))
+        }
+        
+        res.send({mergedData})
+        
     }
 })
 
@@ -64,7 +102,7 @@ app.get('/err', (req, res) => {
 
 app.get('/login', (req, res) => {
     const state = crypto.randomBytes(20).toString('hex');
-    const scope = 'user-read-private user-read-email';
+    const scope = 'user-read-private user-read-email user-top-read';
 
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
@@ -83,15 +121,42 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+app.get('/blend', (req,res) => {
+    const username = req.params.username
 
+
+})
+
+const getUserTopTracks = async (data) => {
+    try {
+        const resp = await axios.get(`https://api.spotify.com/v1/me/top/tracks?limit=50&offset=0`, { headers: {
+            "Authorization": `${data.token_type} ${data.access_token}`
+        }});
+        return resp.data.items
+    } catch (err) {
+        console.log(err)
+        return []
+    }
+}
 const getUserProfile = async (data) => {
     try {
         const resp = await axios.get("https://api.spotify.com/v1/me", { headers: {
             "Authorization": `${data.token_type} ${data.access_token}`
         }});
-        console.log(resp.data);
+        return resp.data
     } catch (err) {
         console.log(err)
-        return { data: {} }
+        return {}
     }
-}
+} 
+const getAudioFeatures = async (data,ids) => {
+    try {
+        const resp = await axios.get(`https://api.spotify.com/v1/audio-features?ids=${ids}`, { headers: {
+            "Authorization": `${data.token_type} ${data.access_token}`
+        }});
+        return resp.data.audio_features
+    } catch (err) {
+        console.log(err)
+        return []
+    }
+} 
